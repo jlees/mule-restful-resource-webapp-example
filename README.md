e7ef0985e514305958e1179ff0ec325b21d72658
= <=>
2016-01-31 20:23:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Mule REST endpoints embedded in Web Application Example
+
+This example demonstrates how to embed Mule defined REST endpoints in a Java web-application (WAR).
+
+## Running the demo
+
+1) Build the application in Maven using "mvn clean install"
+
+2) Deploy the WAR file generated in the previous step to Tomcat.
+
+3) Start Tomcat
+
+4) Open a browser and navigate to http://localhost:8080
+
+5) Click the "Add Person" button add a Person.
+
+6) Click the "View All People" link to view all people.
+
+Based on David Dossot's https://github.com/ddossot/mule-webapp-example
---
+++ src/main/java/org/mule/examples/Person.java
@@ -0,0 +1,46 @@
+/**
+ * MuleSoft Examples
+ * Copyright 2014 MuleSoft, Inc.
+ *
+ * This product includes software developed at
+ * MuleSoft, Inc. (http://www.mulesoft.com/).
+ */
+
+package org.mule.examples;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+
+@JsonAutoDetect
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private Address address;
+	private int age;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
